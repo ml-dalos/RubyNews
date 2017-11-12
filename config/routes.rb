e76e5e3b1bfbcd483a48dc2a4d2938d4ae8be348
ml-dalos <joker.leha98@gmail.com>
2017-11-12 23:18:51 +0300

    Minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'weather/index'
   devise_for :users
   root 'welcome#index'
+  get 'weather/index'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

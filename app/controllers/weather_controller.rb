5be0732bee0b0b2c937b94308ceab740e0914dd2
ml-dalos <joker.leha98@gmail.com>
2017-11-13 21:26:54 +0300

    Add font-awesome gem, change weather/index.html to weather/index.js for AJAX

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,12 +1,55 @@
 class WeatherController < ApplicationController
+
+  DIRECTIONS = %w!N NNE NE ENE E ESE SE SSE S SSW SW WSW W WNW NW NNW!
+
   def index
-    url_location = "http://api.db-ip.com/v2/c19b3558fbbf6b72dffdb99cf85139c7fe0663e7/#{request.ip}"
-    @city = JSON.parse(Net::HTTP.get(URI(url_location)))['city']
-    url_weather = "http://api.openweathermap.org/data/2.5/weather?q=#{@city}&appid=172d1d74437e766c4bd5cde75c58e0da"
-    weather = JSON.parse(Net::HTTP.get(URI(url_weather)))
-    @main = weather['weather'][0]['main']
-    @temperature = weather['main']['temp']
-    @humidity = weather['main']['humidity']
-    @wind = weather['wind']['speed']
+    @weather = parse_weather
+    respond_to do |format|
+      format.js
+    end
   end
+
+  def get_direction(deg)
+    DIRECTIONS[(deg/22.5 + 0.5).round % 16]
+  end
+
+  def get_icon(name)
+    case name
+    when /.*[Rr]ain/
+      'tint'
+    when /.*[Ss]now/
+      'snowflake-o'
+    when /.*[Cc]louds/
+      'cloud'
+    when /.*[Th]understorm/
+      'bolt'
+    when /.*[Mm]ist/
+      'align-justify'
+    when /.*[Cc]lear sky/
+      'sun-o'
+    else
+       'thermometer-half'
+    end
+  end
+
+  private
+
+  def parse_location
+    url_location = "http://api.db-ip.com/v2/c19b3558fbbf6b72dffdb99cf85139c7fe0663e7/#{request.ip}"
+    JSON.parse(Net::HTTP.get(URI(url_location)))['city']
+  end
+
+  def get_weather_json
+    city = parse_location
+    url_weather = "http://api.openweathermap.org/data/2.5/forecast?q=#{city}&mode=json&appid=172d1d74437e766c4bd5cde75c58e0da"
+    JSON.parse(Net::HTTP.get(URI(url_weather)))
+  end
+
+  def parse_weather
+    get_weather_json.
+  end
+
+  def choose_icon main
+  end
+
 end

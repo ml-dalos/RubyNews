fbe6c63e2babba65da22dc30db174e0f0a85d91d
ml-dalos <joker.leha98@gmail.com>
2017-11-15 01:53:50 +0300

    Adding social auth logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'log_in', to: 'sessions#new'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   root 'welcome#index'
   get 'weather', to: 'weather#index'
   get 'welcome/index'

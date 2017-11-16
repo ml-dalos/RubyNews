e9db0bbf1950148bcbb96a9bb712cd297c131900
ml-dalos <joker.leha98@gmail.com>
2017-11-16 17:57:41 +0300

    Add News model and connect it with controllers and User model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,17 @@
 class WelcomeController < ApplicationController
   def index
     redirect_to '/login' unless helpers.current_user
+    @news = params[:source] ? check_source : News.where(source: helpers.current_user.sources)
   end
+
+  private
+
+  def check_source
+    if helpers.current_user.sources.include?(params[:source])
+      News.where(source: params[:source])
+    else
+      redirect_to '/weather'
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114210550) do
+ActiveRecord::Schema.define(version: 20171116142008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "news", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "link"
+    t.string "source"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", null: false

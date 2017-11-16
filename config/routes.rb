e12e633402dbe6d6567151226e10d6d2f3e838a9
ml-dalos <joker.leha98@gmail.com>
2017-11-16 16:30:30 +0300

    Change views and route 'login'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'log_in', to: 'sessions#new'
+  get 'login', to: 'sessions#new'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   root 'welcome#index'

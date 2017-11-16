09e0871a4d855e4308d9f87bea4e130ff8f05e19
ml-dalos <joker.leha98@gmail.com>
2017-11-16 18:54:25 +0300

    Add news partial

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
   root 'welcome#index'
   get 'weather', to: 'weather#index'
-  get 'welcome/index'
+  get 'news', to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
